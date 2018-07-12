189c6924375ac8951ccb6514d046038d49a1427e
Adrian Fernandez <coafer@gmail.com>
2018-07-12 04:30:21 -0400

    finished scafold

--- config/routes.js
+++ config/routes.js
@@ -7,78 +7,78 @@ var scrape = require("../scripts/scrape");
 var headLineController = require("../controllers/headlines");
 var notesController = require("../controllers/notes");
 
-module.exports = function(router){
-    //home page
-    router.get("/", function(req, res){
-        res.render("home");
-    });
+module.exports = function(router) {
+  //home page
+  router.get("/", function(req, res) {
+    res.render("home");
+  });
 
-    //Saved page
-    router.get("/saved", function(req, res){
-        res.render("/saved");
-    });
+  //Saved page
+  router.get("/saved", function(req, res) {
+    res.render("saved");
+  });
 
-    router.get("/api/fetch", function(req, res){
-        headLineController.fetch(function(err, docs){
-            if (!docs || docs.insertedCount === 0){
-                rs.json({
-                    message: "No new articles today. Check out later"
-                });
-            } else {
-                res.json({
-                    message: "Added" + docs.insertedCount + "new artcles"
-                });
-            }
+  router.get("/api/fetch", function(req, res) {
+    headLineController.fetch(function(err, docs) {
+      if (!docs || docs.insertedCount === 0) {
+        rs.json({
+          message: "No new articles today. Check out later"
         });
-    });
-
-    router.get("/api/headlines", function(req, res){
-        var query = {};
-        if (req.query.saved){
-            query = req.query;
-        }
-
-        headLineController.get(query, function(data){
-            res.json(data);
+      } else {
+        res.json({
+          message: "Added" + docs.insertedCount + "new artcles"
         });
+      }
     });
+  });
 
-    router.delete("/api/healines/:id", function(req, res){
-        var query = {};
-        query._id = req.params.id;
-        headLineController.delete(query, function(err, data){
-            res.json(data);
-        });
+  router.get("/api/headlines", function(req, res) {
+    var query = {};
+    if (req.query.saved) {
+      query = req.query;
+    }
+
+    headLineController.get(query, function(data) {
+      res.json(data);
     });
+  });
 
-    router.patch("/api/headlines", function(req, res){
-        headLineController.update(req.body, function(err, data){
-            res.json(data);
-        })
+  router.delete("/api/healines/:id", function(req, res) {
+    var query = {};
+    query._id = req.params.id;
+    headLineController.delete(query, function(err, data) {
+      res.json(data);
     });
+  });
 
-    router.get("/api/notes/:headline_id?", function(req, res){
-        var query ={};
-        if (req.params.headline_id){
-            query._id = req.params.headline_id;
-        }
-
-        notesController.get(query, function(err, data){
-            res.json(data);
-        })
+  router.patch("/api/headlines", function(req, res) {
+    headLineController.update(req.body, function(err, data) {
+      res.json(data);
     });
+  });
 
-    router.delete("/api/notes/:id", function(req, res){
-        var query ={};
-        query._id = req.params.id;
-        notesController.delete(query, function(err, data){
-            res.json(data);
-        });
-    });
+  router.get("/api/notes/:headline_id?", function(req, res) {
+    var query = {};
+    if (req.params.headline_id) {
+      query._id = req.params.headline_id;
+    }
 
-    router.post("/api/notes", function(req, res){
-        notesController.save(req.body, function(data){
-            res.json(data);
-        })
+    notesController.get(query, function(err, data) {
+      res.json(data);
     });
-} 
+  });
+
+  router.delete("/api/notes/:id", function(req, res) {
+    var query = {};
+    query._id = req.params.id;
+    notesController.delete(query, function(err, data) {
+      res.json(data);
+    });
+  });
+
+  router.post("/api/notes", function(req, res) {
+    notesController.save(req.body, function(data) {
+      res.json(data);
+    });
+  });
+};
--- public/javascript/saved.js
+++ public/javascript/saved.js
@@ -1,62 +1,123 @@
-$(document).ready(function(){
-    var articleContainer = $(".article-container");
+$(document).ready(function() {
+  var articleContainer = $(".article-container");
 
-    $(document).on("click", ".btn.delete", handleArticleDelete);
-    $(document).on("click", ".btn.notes", handleArticleNotes);
-    $(document).on("click", ".btn.save", handleArticleSave);
-    $(document).on("click", ".btn.note-delete", handleNoteDelete);
+  $(document).on("click", ".btn.delete", handleArticleDelete);
+  $(document).on("click", ".btn.notes", handleArticleNotes);
+  $(document).on("click", ".btn.save", handleArticleSave);
+  $(document).on("click", ".btn.note-delete", handleNoteDelete);
 
-    //Run the init page function
-    initPage();
+  //Run the init page function
+  initPage();
 
-    function initPage(){
-        //Empty the article container, run an Ajax request for any unsaved headlines
-        articleContainer.empty();
-        $.get("/api/headlines?saved=true")
-            .then(function(data){
-                //if there are headlines render them
-                if (data && data.length){
-                    renderArticles(data);
-                } else {
-                    renderEmpty();
-                }
-            });
+  function initPage() {
+    //Empty the article container, run an Ajax request for any unsaved headlines
+    articleContainer.empty();
+    $.get("/api/headlines?saved=true").then(function(data) {
+      //if there are headlines render them
+      if (data && data.length) {
+        renderArticles(data);
+      } else {
+        renderEmpty();
+      }
+    });
+  }
+
+  function renderArticles(articles) {
+    //Appends HTML containing the article data to de the page
+    //The array goes with a JSON containing all available articles in the db
+    var articlePanels = [];
+    //We pass ecah article to createPanels function wich returns a bootstrap panel with the aricles inside
+    for (var i = 0; i < articles.length; i++) {
+      articlePanels.push(createPanel(articles[i]));
     }
+    articleContainer.append(articlePanels);
+  }
 
-    function renderArticles(articles){
-        //Appends HTML containing the article data to de the page
-        //The array goes with a JSON containing all available articles in the db
-        var articlePanels = [];
-        //We pass ecah article to createPanels function wich returns a bootstrap panel with the aricles inside
-        for (var i = 0; i < articles.length; i++){
-            articlePanels.push(createPanel(articles[i]));
-        }
-        articleContainer.append(articlePanels);
+  function handleArticleDelete() {
+    var articleToDelete = $(this)
+      .parents(".panel")
+      .data();
+
+    $.ajax({
+      method: "DELETE",
+      url: "/api/headlines" + articleToDelete._id
+    }).then(function(data) {
+      if (data.ok) {
+        initPage();
+      }
+    });
+  }
+
+  function handleArticleNotes() {
+    //this function handle openning the notes modal and displaying our notes
+    var currentArticle = $(this)
+      .parents(".panel")
+      .data();
+    //grab any note withi this id
+    $.get("/api/notes" + currentArticle._id).then(function(data) {
+      var modalText = [
+        "<div class='container-fluid text-center'>",
+        "<h4>Notes for article: ",
+        currentArticle._id,
+        "</h4>",
+        "<hr />",
+        "<ul class='list-group note-container'>",
+        "</ul>",
+        "<textarea placeholder='New Note' rows='4' cols='60'></textarea>",
+        "<button class='btn btn-success save'>Save Note</button>",
+        "</div>"
+      ].join("");
+
+      botbox.dialog({
+        message: modalText,
+        closeButton: true
+      });
+
+      var noteData = {
+        _id: currentArticle._id,
+        notes: data || []
+      };
+
+      $(".btn.save").data("article", noteData);
+
+      renderNotesList(noteData);
+    });
+  }
+
+  function renderNotesList(data) {
+    var notesToRender = [];
+    var currentNote;
+    if (!data.notes.length) {
+      currentNote = [
+        "<li class='list-group-item'>",
+        "No notes for this article",
+        "</li>"
+      ].join("");
+      notesToRender.push(currentNote);
+    } else {
+      for (let i = 0; i < data.notes.length; i++) {
+        currentNote = $(
+          [
+            "<li class='list-group-item'>",
+            data.notes[i].noteText,
+            "<button class='btn btn-danger note-delete'>Close</button>",
+            "</li>"
+          ].join("")
+        );
+        currentNote.children("button").data("_id", data.notes[i]._id);
+        notesToRender.push(currentNote);
+      }
     }
+    $(".note-container").append(notesToRender);
+  }
 
-    function handleArticleDelete(){
-        var articleToDelete = $(this).parents(".panel").data();
-
-        $.ajax({
-            method: "DELETE",
-            url: "/api/headlines" + articleToDelete._id
-        })
-        .then(function(data){
-            if(data.ok){
-                initPage();
-            }
-        });
-    }
-
-    function handleArticleNotes(){
-        //------------------------------35:41
-    }
-
-    function createPanel(article){
-        //this function construct a jQuery element for the articles panel
-        var panel = 
-        $(["<div class='panel panel-default'>",
-          "<div class='panel-heading'>", "<h3>",
+  function createPanel(article) {
+    //this function construct a jQuery element for the articles panel
+    var panel = $(
+      [
+        "<div class='panel panel-default'>",
+        "<div class='panel-heading'>",
+        "<h3>",
         article.headline,
         "<a class='btn btn-success save'>",
         "Save Article",
@@ -66,21 +127,50 @@ $(document).ready(function(){
         "<div class='panel-body'>",
         article.summary,
         "</div>",
-        "</div>"].join(""));
-        //We attach the article's id to the jQuery element
-        panel.data("_id", article._id);
-        //we return the constructed panel jQuery element
-        return panel;
-    }
+        "</div>"
+      ].join("")
+    );
+    //We attach the article's id to the jQuery element
+    panel.data("_id", article._id);
+    //we return the constructed panel jQuery element
+    return panel;
+  }
 
-    function renderEmpty(){
-        var emptyAlert = 
-            $([
-                "<div class='alert alert-warning text-center'>",
-                "<h4>We don't have any new artcles</h4>",
-                "</div>"
-            ].join(""));
-
-        articleContainer.append(emptyAlert);
+  function handleNoteSave() {
+    var noteData;
+    var newNote = $(".bootbox-body textarea")
+      .val()
+      .trim();
+    if (newNote) {
+      noteData = {
+        _id: $(this).data("article")._id,
+        noteText: newNote
+      };
+      $.post("/api/notes", noteData).then(function() {
+        bootbox.hideAll();
+      });
     }
+  }
+
+  function renderEmpty() {
+    var emptyAlert = $(
+      [
+        "<div class='alert alert-warning text-center'>",
+        "<h4>We don't have any new artcles</h4>",
+        "</div>"
+      ].join("")
+    );
+
+    articleContainer.append(emptyAlert);
+  }
 });
+
+function handleNoteDelete() {
+  var noteToDelete = $(this).data("_id");
+  $.ajax({
+    url: "/api/notes/" + noteToDelete,
+    method: "DELETE"
+  }).then(function() {
+    bootbox.hideAll();
+  });
+}
